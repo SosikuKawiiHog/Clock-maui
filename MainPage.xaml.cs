fb2952a7caa3e0e9ee43e4978efb5d59c79487b9
SosikuKawiiHog <razdobarov_se@edu.surgu.ru>
2025-10-05 19:23:46 +0500

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Clock
+{
+    public partial class MainPage : ContentPage
+    {
+        private DateTime _currTime;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeClock();
+        }
+
+        private void InitializeClock()
+        {
+            _currTime = DateTime.Now;
+            UpdateTimeDisplay();
+        }
+
+        private void UpdateTimeDisplay()
+        {
+            TimeLabel.Text = _currTime.ToString("HH:mm:ss");
+        }
+    }
+}
